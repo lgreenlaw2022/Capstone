6d232710d19acc1d1de6f59a58ef663baa87ace4
Libby Greenlaw <lgreenlaw@uni.minerva.edu>
2024-09-17 22:00:37 +0200

    Added fonts and header contents

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,26 @@
 import styles from '@/styles/Header.module.css';
+import Image from 'next/image';
 
 export default function Header() {
     return (
-        <div className={styles.header}>Header</div>
+        <div className={styles.header}>
+            <div className={styles.title}>
+                <Image src="/assets/favicon.ico" height={30} width={30} alt="favicon" />
+                <h1>AlgoArena</h1>
+            </div>
+            <div className={styles.stats}>
+                {/* Add calls or props to retrieve values -- likely useEffect */}
+                <div className={styles.statItem}>
+                    <Image src="/assets/favicon.ico" height={26} width={26} alt="favicon" />
+                    <h3>1</h3>
+                </div>
+                <div className={styles.statItem}>
+                    <Image src="/assets/favicon.ico" height={26} width={26} alt="favicon" />
+                    <h3>233</h3>
+                </div>
+                {/* add prop to import username */}
+                <h3>Libby Green</h3>
+            </div>
+        </div>
     );
 }
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,7 +2,23 @@ import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import Footer from "@/components/Footer";
 import "@/styles/globals.css";
+import { Roboto } from 'next/font/google';
+import { Roboto_Condensed } from 'next/font/google';
 import type { AppProps } from "next/app";
+
+const roboto = Roboto({
+  weight: ['400', '500', '700', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const robotoCondensed = Roboto_Condensed({
+  weight: ['400','600','700','800'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
