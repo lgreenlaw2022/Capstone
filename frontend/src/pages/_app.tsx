6cf57245f9f1e73f1f8378d251803d985afe066b
Libby Greenlaw <lgreenlaw@uni.minerva.edu>
2024-09-19 14:55:43 +0200

    added redirect logic to index page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,30 +5,36 @@ import "@/styles/globals.css";
 import { Roboto } from 'next/font/google';
 import { Roboto_Condensed } from 'next/font/google';
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 const roboto = Roboto({
-  weight: ['400', '500', '700', '900'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
+	weight: ['400', '500', '700', '900'],
+	style: ['normal', 'italic'],
+	subsets: ['latin'],
+	display: 'swap',
 });
 
 const robotoCondensed = Roboto_Condensed({
-  weight: ['400','600','700','800'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
+	weight: ['400', '600', '700', '800'],
+	style: ['normal'],
+	subsets: ['latin'],
+	display: 'swap',
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Header />
-      <div className="main-layout">
-        <Sidebar />
-        <Component {...pageProps} />
-      </div>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>AlgoArena</title>
+				<meta name="description" content="EdTech app for technical interview preparation" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<Header />
+			<div className="main-layout">
+				<Sidebar />
+				<Component {...pageProps} />
+			</div>
+			<Footer />
+		</>
+	);
 }
