1711b00c847c891d34b73a4e339d70ff70cdb079
Libby Greenlaw <lgreenlaw@uni.minerva.edu>
2024-09-19 21:20:38 +0200

    added header icons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,25 +1,28 @@
 import styles from '@/styles/Header.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
     return (
         <div className={styles.header}>
             <div className={styles.title}>
-                <Image src="/assets/favicon.ico" height={30} width={30} alt="favicon" />
+                <Image src="/assets/favicon.ico" height={30} width={30} alt="AlgoArena logo" />
                 <h1>AlgoArena</h1>
             </div>
             <div className={styles.stats}>
                 {/* Add calls or props to retrieve values -- likely useEffect */}
                 <div className={styles.statItem}>
-                    <Image src="/assets/favicon.ico" height={26} width={26} alt="favicon" />
+                    <Image src="/assets/flame.svg" height={26} width={26} alt="streak flame" />
                     <h3>1</h3>
                 </div>
                 <div className={styles.statItem}>
-                    <Image src="/assets/favicon.ico" height={26} width={26} alt="favicon" />
+                    <Image src="/assets/gem.svg" height={26} width={26} alt="gem" />
                     <h3>233</h3>
                 </div>
                 {/* add prop to import username */}
-                <h3>Libby Green</h3>
+                <Link href="/profile">
+                    <h3 className={styles.profileLink}>Libby Green</h3>
+                </Link>
             </div>
         </div>
     );
