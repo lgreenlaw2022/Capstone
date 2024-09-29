3740b257e2d706168662d7e121b5148cf89e3731
Libby Greenlaw <lgreenlaw@uni.minerva.edu>
2024-09-29 19:53:31 +0200

    added props to header values

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react';
 import styles from '@/styles/Header.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 
+interface UserData {
+    username: string;
+    streakCount: number;
+    gemCount: number;
+}
+
 export default function Header() {
+    const [userData, setUserData] = useState<UserData | null>({ username: 'Libby Green', streakCount: 1, gemCount: 233 });
+
+    // TODO: update with actual API call
+    // TODO: will need to make sure it is called whenever values change
+    // useEffect(() => {
+    //     // Fetch user data from the API
+    //     fetch('/api/user')
+    //         .then(response => response.json())
+    //         .then(data => setUserData(data))
+    //         .catch(error => console.error('Error fetching user data:', error));
+    // }, []);
+
+    // TODO: switch to a custom spinner component
+    if (!userData) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div className={styles.header}>
             <div className={styles.title}>
@@ -13,15 +37,15 @@ export default function Header() {
                 {/* Add calls or props to retrieve values -- likely useEffect */}
                 <div className={styles.statItem}>
                     <Image src="/assets/flame.svg" height={26} width={26} alt="streak flame" />
-                    <h3>1</h3>
+                    <h3>{userData.streakCount}</h3>
                 </div>
                 <div className={styles.statItem}>
                     <Image src="/assets/gem.svg" height={26} width={26} alt="gem" />
-                    <h3>233</h3>
+                    <h3>{userData.gemCount}</h3>
                 </div>
                 {/* add prop to import username */}
                 <Link href="/profile">
-                    <h3 className={styles.profileLink}>Libby Green</h3>
+                    <h3 className={styles.profileLink}>{userData.username}</h3>
                 </Link>
             </div>
         </div>
