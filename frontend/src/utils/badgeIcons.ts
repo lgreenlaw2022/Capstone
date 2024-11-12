d0a02c259b1133f3973bcce16db2f9312df3fc0e
Libby Greenlaw <lgreenlaw@uni.minerva.edu>
2024-11-12 16:38:17 +0100

    added quiz master icon

--- frontend/src/utils/badgeIcons.ts
+++ frontend/src/utils/badgeIcons.ts
@@ -1,6 +1,7 @@
 const badgeIcons: { [key: string]: string } = {
     "Hash Tables": "/assets/unit-icons/hashmaps.svg",
     "30 day streak": "/assets/award-icons/flame.svg",
+    "Quiz Master": "/assets/award-icons/100.svg",
 };
 
 export default badgeIcons;
