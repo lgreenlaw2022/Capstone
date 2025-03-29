88e830792529cd6f1dcc3f9d90cfda11b9547b93
Libby Greenlaw <lgreenlaw@uni.minerva.edu>
2025-03-29 21:04:56 +0100

    improve weekly review question population and comments

--- backend/routes/review.py
+++ backend/routes/review.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models import Unit, User, db, UserModule, QuizQuestion, UserQuizQuestion, Module
 import random
+from typing import List, Dict, Any
 from enums import ModuleType
 from services.user_activity_service import UserActivityService
 from constants import XP_FOR_COMPLETING_REVIEW, QUIZ_ACCURACY_THRESHOLD
@@ -16,11 +17,11 @@ logger.setLevel(logging.DEBUG)
 user_activity_service = UserActivityService()
 
 
+
 @review_bp.route("/weekly-review/questions", methods=["GET"])
 @jwt_required()
 def get_weekly_review():
     try:
-        logger.debug("Fetching weekly review data")
         user_id = get_jwt_identity()
         weekly_review_questions = pick_weekly_review_questions(user_id)
         return jsonify(weekly_review_questions), 200
@@ -29,20 +30,33 @@ def get_weekly_review():
         return jsonify({"error": str(e)}), 500
 
 
-def pick_weekly_review_questions(user_id):
+def pick_weekly_review_questions(user_id: int) -> List[Dict[str, Any]]:
+    """
+    Selects a set of weekly review questions for a user based on their recent activity.
+
+    The function prioritizes questions from the most recently completed unit (65% of the total)
+    and interleaves questions from other practiced units (35% of the total), weighted by the time since
+    they were last practiced. If there are not enough questions, it adjusts accordingly.
+    This is an implementation of spaced practice.
+
+    Args:
+        user_id (int): The ID of the user for whom the review questions are being selected.
+
+    Returns:
+        List[Dict[str, Any]]: A list of serialized question data, including question ID, title,
+                              and options (with IDs, text, and correctness).
+    """
     TOTAL_QUESTIONS = 10
-    MOST_RECENT_PERCENT = 0.65
+    MOST_RECENT_QUESTIONS_PERCENT = 0.65
 
     # Get the current date and the date 3 months ago
     current_date = datetime.now()
     three_months_ago = current_date - timedelta(days=90)
 
     # Fetch all questions the user has practiced within the past 3 months
-    logger.debug(f"Fetching practiced questions for user {user_id}")
     practiced_questions = fetch_practiced_questions(user_id, three_months_ago)
 
     # Identify the most recently completed quiz module
-    # Note. the completed date is based on original completion date, not the last practiced date
     most_recent_module = (
         db.session.query(UserModule)
         .join(Module, UserModule.module_id == Module.id)
@@ -60,34 +74,34 @@ def pick_weekly_review_questions(user_id):
         logger.debug("User does not have any questions to review")
         return []
 
-    logger.debug(
-        f"Most recently completed module: {most_recent_module.module_id}, selecting questions for review"
-    )
     most_recent_unit_id = most_recent_module.module.unit_id
-    # interleave questions to review from different units and quiz modules
-    # the algorithm prioritizes newly learned topics and interleaves old/unreviewed questions for spaced practice
-    # Select 65% of questions from the most recently practiced unit
-    most_recent_questions = [
-        q for q in practiced_questions if q["unit_id"] == most_recent_unit_id
-    ]
-    num_most_recent_questions = int(MOST_RECENT_PERCENT * TOTAL_QUESTIONS)
-    selected_questions = random.sample(
-        most_recent_questions,
-        min(num_most_recent_questions, len(most_recent_questions)),
-    )
-    logger.debug(
-        f"Selected {len(selected_questions)} questions from the most recent unit"
-    )
 
-    # Select the remaining 35% of questions from other units, weighted by the longest date since they were last reviewed
-    remaining_questions = [
+    # Select 35% of questions from older units
+    diff_unit_questions = [
         q for q in practiced_questions if q["unit_id"] != most_recent_unit_id
     ]
-    remaining_questions.sort(key=lambda q: q["last_practiced_date"])
-    num_remaining_questions = TOTAL_QUESTIONS - len(selected_questions)
-    selected_questions.extend(remaining_questions[:num_remaining_questions])
-    logger.debug(f"Selecting {num_remaining_questions} questions from others")
-    # TODO: if there are not enough other questions, add more from the most recent unit
+    # Sort by the last practiced date (ascending) to prioritize less fresh questions
+    diff_unit_questions.sort(key=lambda q: q["last_practiced_date"])
+    num_diff_unit_questions = int((1 - MOST_RECENT_QUESTIONS_PERCENT) * TOTAL_QUESTIONS)
+    selected_questions = diff_unit_questions[:num_diff_unit_questions]
+
+    # If there are not enough questions from other units, adjust the number of remaining questions
+    num_diff_unit_selected = len(selected_questions)
+    num_remaining_questions = TOTAL_QUESTIONS - num_diff_unit_selected
+
+    # Fill the remaining questions with randomly selected questions from the most recently practiced unit
+    most_recent_unit_questions = [
+        q for q in practiced_questions if q["unit_id"] == most_recent_unit_id
+    ]
+    # Either 65% from the most recent unit or more if needed
+    selected_questions.extend(
+        random.sample(
+            most_recent_unit_questions,
+            min(
+                num_remaining_questions, len(most_recent_unit_questions)
+            ),  # ensure not to exceed the available questions
+        )
+    )
 
     # shuffle the questions
     random.shuffle(selected_questions)
@@ -95,6 +109,8 @@ def pick_weekly_review_questions(user_id):
     # serialize the question data for chosen questions
     questions = []
     for question in selected_questions:
+        # Fetch the complete question object from the database
+        # this is necessary to get the options for the question
         question_obj = (
             db.session.query(QuizQuestion).filter_by(id=question["question_id"]).first()
         )
@@ -114,7 +130,23 @@ def pick_weekly_review_questions(user_id):
     return questions
 
 
-def fetch_practiced_questions(user_id, date_limit):
+def fetch_practiced_questions(
+    user_id: int, date_limit: datetime
+) -> List[Dict[str, Any]]:
+    """
+    Fetches all quiz questions and their relevant metadata that a user has practiced since a specified date.
+
+    Args:
+        user_id (int): The ID of the user whose practiced questions are being fetched.
+        date_limit (datetime): The earliest date to include questions that were practiced.
+
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries, where each dictionary contains:
+            - question_id (int): The ID of the quiz question.
+            - module_id (int): The ID of the module the question belongs to.
+            - unit_id (int): The ID of the unit the question belongs to.
+            - last_practiced_date (datetime): The last date the question was practiced.
+    """
     practiced_questions = (
         db.session.query(UserQuizQuestion)
         .join(UserQuizQuestion.quiz_question)
@@ -163,6 +195,7 @@ def get_weekly_review_status():
 @jwt_required()
 def submit_weekly_review():
     try:
+        user_id = get_jwt_identity()
         data = request.get_json()
         accuracy = data.get("accuracy")
         # Validate accuracy
@@ -173,7 +206,6 @@ def submit_weekly_review():
 
         # mark module as complete if accuracy is above passing threshold
         if accuracy >= QUIZ_ACCURACY_THRESHOLD:
-            user_id = get_jwt_identity()
             user = User.query.get(user_id)
             user.weekly_review_done = True
             user_activity_service.update_daily_xp(user_id, XP_FOR_COMPLETING_REVIEW)
@@ -197,8 +229,7 @@ def get_unit_review_questions(unit_id):
         if not unit:
             return jsonify({"error": "Unit not found"}), 404
 
-        logger.debug(f"Fetching unit review data for unit {unit_id}")
-
+        # Fetch all quiz questions for the unit
         unit_questions = (
             db.session.query(QuizQuestion)
             .join(Module)
@@ -265,7 +296,19 @@ def submit_unit_quiz_score(unit_id):
         return jsonify({"error": str(e)}), 500
 
 
-def update_quiz_questions_practiced_date(user_id, questions):
+def update_quiz_questions_practiced_date(
+    user_id: int, questions: List[QuizQuestion]
+) -> None:
+    """
+    Updates the last practiced date for a list of quiz questions for a specific user to today.
+
+    Args:
+        user_id (int): The ID of the user whose quiz question records are being updated.
+        questions (List[QuizQuestion]): A list of `QuizQuestion` objects to update.
+
+    Returns:
+        None
+    """
     for question in questions:
         # create UserQuizQuestion record if it does not exist
         user_question = UserQuizQuestion.query.filter_by(
